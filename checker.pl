3f5f0a810a674acc3a779080448422abcfce9896
Joseph Jezak josejx@gentoo.org <josejx@gentoo.org>
2014-04-17 15:26:20 -0400

    Updated to use Klusark's new format.

--- checker.pl
+++ checker.pl
@@ -2,62 +2,157 @@
 use strict;
 use Data::Dumper;
 
-my $golden_vector_list = "retailoutput.txt";
-my $test_vector_list = "testoutput.txt";
+if($#ARGV != 1) {
+	print ("Usage: checker.pl <golden vectors> <test vectors>\n");
+	exit(-1);
+}
+
 my $tolerance = 0.005;
+my $golden_vector_list = $ARGV[0];
+my $test_vector_list = $ARGV[1];
 
 ### Open the golden vector list and read it in
-open (GF, $golden_vector_list);
+open (GF, $golden_vector_list) or die "Can't open the file: ".$ARGV[0]."\n";
 my @gf = <GF>;
+chomp(@gf);
 close (GF);
+my $gv = parse_test_file(\@gf);
 
 ### Open the test file
-open (TF, $test_vector_list);
+open (TF, $test_vector_list) or die "Can't open the file: ".$ARGV[1]."\n";
 my @tf = <TF>;
+chomp(@tf);
 close (TF);
+my $tv = parse_test_file(\@tf);
 
-my $test_id = 0;
-my $errors = 0;
-my $tf_idx = 0;
-my $print_line = 0;
-my $count = 0;
-foreach my $line (@tf) {
-	### Start/End
-	if ($line =~ m/test(\d+)\s+(\S+)/) {
-		$test_id = $1;	
-		if ($2 eq "start") {
-			$errors = 0;
-			$count = 1;
-			print ("Checking test$test_id...\n");
-		} else {
-			print ("Done with test$test_id: $errors errors out of $count tests\n");
-		}
-	### Parse the line
-	} else {
-		### Remove the parens
-		chomp($line);
-		$line =~ s/[\(\)]//g;
-		my $gv_line = $gf[$tf_idx];
-		chomp($gv_line);
-		$gv_line =~ s/[\(\)]//g;
+### Check the two vector lists
+compare_results($gv, $tv);
 
-		### Split into vectors
-		my @tv = split(/,/, $line);
-		my @gv = split(/,/, $gv_line);
+###############################################################################
+### Support Functions
+###############################################################################
+sub coords {
+	my $line = shift;
 
-		### Expecting 3 elements
-		for (my $i=0; $i<3; $i++) {
-			### Error!
-			if(abs($tv[$i] - $gv[$i]) > $tolerance) {
-				$print_line = 1;
+	$line =~ s/[\(\)]//g;
+	my @vec = split(/,/, $line);
+	return \@vec;
+}
+
+sub parse_test_file {
+	my @data = @{shift @_};
+	my $results = {};
+	my $test_id;
+	my $test_count = 0;
+	my $test_set;
+
+	for (my $idx=0; $idx<$#data+1;) {
+		my $line = $data[$idx];
+		### Start/End
+		if ($line =~ m/^test(\d+)\s+(\S+)/) {
+			$test_id = $1;
+			if ($2 eq "start") {
+				$test_count = 1;
+				$test_set = [];
+			} elsif ($2 eq "end") {
+				$results->{'test'.$test_id} = $test_set;
+				undef($test_id);
 			}
+		### Parse an attach test
+		} elsif ($line =~ m/attachTest with (\d+) actors/) {
+			my $test = {};
+			my $actors = $1;
+
+			### Read in the actor positions
+			for(my $i=0; $i<3; $i++) {
+				$idx++;
+				$test->{'actor'.$i} = coords($data[$idx]);
+			}
+
+			### Skip the result line
+			$idx++;
+			if (not $data[$idx] =~ m/^result/) {
+				print "ERROR: unexpected input: ", $data[$idx], "\n";
+				exit;
+			}
+
+			### Read in the result
+			$idx++;
+			$test->{'attached'} = coords($data[$idx]);
+			$idx++;
+			$test->{'detached'} = coords($data[$idx]);
+
+			push @{$test_set}, $test;
+			$test_count++;
 		}
-		if($print_line) {
-			$errors++;
-			print "Test ", $count, ": ", $gv_line, " != ", $line, "\n";
-			$print_line = 0;
-		}
-		$count++;
+		$idx++;
 	}
-	$tf_idx++;
+	return $results;
+}
+
+### Check if two vectors are equal within tolerance
+sub equal {
+	my $gv = shift;
+	my $tv = shift;
+
+	### 3 elements in the vector
+	for (my $i=0; $i<3; $i++) {
+		### Error!
+		if(abs($tv->[$i] - $gv->[$i]) > $tolerance) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+### Return a printable vector
+sub pv {
+	my $vec = shift;
+	return "(" . $vec->[0] . "," . $vec->[1] . "," . $vec->[2] . ")";
+}
+
+### Compare the results between two tests
+sub compare_results {
+	my $attached_errors = 0;
+	my $detached_errors = 0;
+	my $print_line = 0;
+	### Good vector list
+	my %gv = %{shift(@_)};
+	### Test vector list
+	my %tv = %{shift(@_)};
+	my $testrun;
+
+	foreach $testrun (keys(%gv)) {
+		print "Checking ",$testrun,"\n";
+		$attached_errors = 0;
+		$detached_errors = 0;
+		my @g_tests = @{$gv{$testrun}};
+		my @t_tests = @{$tv{$testrun}};
+
+		my $test_idx = 0;
+		foreach my $t (@g_tests) {
+			my %gold = %{$t};
+			my %test = %{$t_tests[$test_idx]};
+
+			### Check that the tests match
+			if (not (equal($gold{'actor0'}, $test{'actor0'}) and equal($gold{'actor1'}, $test{'actor1'}) and equal($gold{'actor2'}, $test{'actor2'}))) {
+				print "Test files don't test the same thing: Error with test $test_idx\n";
+				exit;
+			}
+
+			### Check that the attached matches
+			if (not equal($gold{'attached'}, $test{'attached'})) {
+				$attached_errors++;
+				print ("Error in Test " . ($test_idx + 1) . " Attached: A1:" . pv($gold{'actor0'}) . " A2: " . pv($gold{'actor1'}) . " A3: " . pv($gold{'actor2'}). "\n\t". pv($gold{'attached'}) . " != " . pv($test{'attached'}). "\n");
+			}
+
+			### Check that the detached matches
+			if (not equal($gold{'detached'}, $test{'detached'})) {
+				$detached_errors++;
+				print("Error in Test " . ($test_idx + 1) . " Detached: A1:" . pv($gold{'actor0'}) . " A2: " . pv($gold{'actor1'}) . " A3: " . pv($gold{'actor2'}). "\n\t". pv($gold{'detached'}) . " != " . pv($test{'detached'}). "\n");
+			}
+			$test_idx++;
+		}
+		print "$testrun completed with $attached_errors attachment errors and $detached_errors detachment errors.\n";
+	}
 }
