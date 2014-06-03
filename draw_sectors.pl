974666ac5569b62ce1a6de98409b8d56c424f990
Joseph Jezak josejx@gentoo.org <josejx@gentoo.org>
2014-06-02 22:15:31 -0400

    Add writing the sector ID #'s to the figure.

--- draw_sectors.pl
+++ draw_sectors.pl
@@ -2,6 +2,93 @@
 use strict;
 use Data::Dumper;
 
+###############################################################################
+### Subroutines
+###############################################################################
+### Color code to avoid duplication
+sub get_color {
+	my $type = shift;
+
+	### WALK - Blue
+	if ($type eq "walk") {
+		return 1;
+	### FUNNEL - Yellow
+	} elsif ($type eq "funnel") {
+		return 6;
+	### CAMERA - Black
+	} elsif ($type eq "camera") {
+		return 0;
+	### SPECIAL - Magenta
+	} elsif ($type eq "special") {
+		return 5;
+	### HOT - Red
+	} elsif ($type eq "hot" or $type eq "chernobyl") {
+		return 4;
+	} else {
+		print "ERROR: Unknown type: ",$type,"\n";
+		return 0;
+	}
+	return 0; ### Shouldn't get here
+}
+
+### Find the center of a bounding box
+sub bb_center {
+	my @verts = @{shift()};
+	my ($x, $y, $nx, $ny);
+	my ($cx, $cy) = (0, 0);
+	my $area = 0;
+
+	my ($lowX, $lowY, $highX, $highY) = (99999.0, 99999.9, -99999.9, -99999.9);
+	for (my $i=0; $i<$#verts; $i++) {
+		if ($verts[$i]->[0] > $highX) {
+			$highX = $verts[$i]->[0];
+		}
+		if ($verts[$i]->[0] < $lowX) {
+			$lowX = $verts[$i]->[0];
+		}
+		if ($verts[$i]->[1] > $highY) {
+			$highY = $verts[$i]->[1];
+		}
+		if ($verts[$i]->[1] < $lowY) {
+			$lowY = $verts[$i]->[1];
+		}
+	}
+
+	$cx = int($lowX + ($highX - $lowX)/2);
+	$cy = int($lowY + ($highY - $lowY)/2);
+	return ($cx, $cy);
+}
+
+### Convert coordinates to XFIG coordinates
+sub conv_verts {
+	my @new_verts;
+	my @verts = @{shift()};
+	my $scale = shift();
+	my @offset = @{shift()};
+	my $game = shift();
+
+	### Loop over the verts
+	for (my $i=0; $i<$#verts + 1; $i++) {
+		my $x = $offset[0] + int($verts[$i]->[0] * 1000);
+		my $y;
+		### EMI
+		if ($game eq "EMI") {
+			$y = $offset[2] + int($verts[$i]->[2] * 1000);
+		### GRIM
+		} else {
+			$y = $offset[1] + int($verts[$i]->[1] * 1000);
+		}
+		push @new_verts, [int($scale * $x), int($scale * $y)];
+	}
+	### Add the start to the list
+	push @new_verts, [ $new_verts[0]->[0], $new_verts[0]->[1] ];
+	return \@new_verts;
+}
+
+###############################################################################
+### Main code
+###############################################################################
+
 ### Check for params
 if ($#ARGV < 1) {
 	print "Usage:\n\tdraw_sectors.pl <game type> <set file> <optional filter>\n\n";
@@ -45,7 +132,7 @@ for (my $pos=0; $pos<$#set+1; $pos++) {
 
 		### Loop until a whole line is just whitespace
 		while ($set[$pos] !~ m/^\s*$/) {
-			$line = $set[$pos];	
+			$line = $set[$pos];
 
 			if ($line =~ m/type\s+(\w*)/) {
 				$sectors{$id}->{'type'} = $1;
@@ -74,7 +161,7 @@ for (my $pos=0; $pos<$#set+1; $pos++) {
 						$lowest[$j] = $v[$j] if ($v[$j] < $lowest[$j]);
 						$highest[$j] = $v[$j] if ($v[$j] > $highest[$j]);
 					}
-					
+
 					$pos++;
 					$line = $set[$pos];
 				}
@@ -89,8 +176,8 @@ for (my $pos=0; $pos<$#set+1; $pos++) {
 }
 close SET;
 
-### Adjust the values so we can shift the points
-my $max = 0;
+### Adjust the high and low values into ints so we can shift the actual verts
+### so that there are no negative verts
 for (my $i=0; $i<3; $i++) {
 	$lowest[$i] = int($lowest[$i] * 1000);
 	if ($lowest[$i] > 0) {
@@ -100,9 +187,35 @@ for (my $i=0; $i<3; $i++) {
 	}
 
 	$highest[$i] = $lowest[$i] + int($highest[$i] * 1000);
-	if ($highest[$i] > $max) {
-		$max = $highest[$i];
+}
+
+### Set the scale - 1200ppi
+### US Letter is 8.5x11 inches
+my $xdim = 11 * 1200;
+my $ydim = 8.5 * 1200;
+
+### Scaling
+my $xs = 1.0;
+if ($highest[0] > $xdim)  {
+	$xs = $xdim / $highest[0];
+}
+my $ys;
+if ($game eq "EMI") {
+	if ($highest[2] > $ydim) {
+		$ys = $ydim / $highest[2];
 	}
+} else {
+	if ($highest[1] > $ydim) {
+		$ys = $ydim / $highest[1];
+	}
+}
+
+my $scale = 1.0;
+if ($xs < $scale) {
+	$scale = $xs;
+}
+if ($ys < $scale) {
+	$scale = $ys;
 }
 
 ### Convert the sector list to XFIG polygons
@@ -110,109 +223,84 @@ open XFIG, ">".$ARGV[1].".fig" or die "Can't open the xfig file for writing.\n";
 print XFIG "#FIG 3.2\n";
 print XFIG "Landscape\n";
 print XFIG "Center\n";
-print XFIG "Metric\n";
-print XFIG "A4\n";
+print XFIG "Inches\n";
+print XFIG "Letter\n";
 print XFIG "100.00\n";
 print XFIG "Single\n";
 print XFIG "-2\n";
-
-### Set the scale
-### FIXME: This works, but it's kind of voodoo magic at the moment
-print XFIG int(1300 * ($max / 10000)), " 2\n";
+print XFIG "1200 2\n";
 
 ### All sectors will be represented by polylines
 foreach my $k (keys %sectors) {
-	my %sector = %{$sectors{$k}};	
+	my %sector = %{$sectors{$k}};
 
 	### Filter?
 	next if (defined($filter) and $sector{type} ne $filter);
 
 	### Add a comment with the name
-	print XFIG "# ",$sector{name},"\n";
-	
+	print XFIG "# ",$sector{name}," ($k)\n";
+
 	### Create the polyline object (type 2), type is polygon (type 3)
 	print XFIG "2 3 ";
-	
 	### Determine visibility, solid if visible, dashed if not, thickness is always 1
 	if ($sector{visibility}) {
 		print XFIG "0 1 ";
 	} else {
 		print XFIG "1 1 ";
 	}
-
 	### Determine the pen and fill color based on the type
-	### WALK - Blue
-	if ($sector{type} eq "walk") {
-		print XFIG "1 1 ";
-	### FUNNEL - Yellow
-	} elsif ($sector{type} eq "funnel") {
-		print XFIG "6 6 ";
-	### CAMERA - Black
-	} elsif ($sector{type} eq "camera") {
-		print XFIG "0 0 ";
-	### SPECIAL - Magenta
-	} elsif ($sector{type} eq "special") {
-		print XFIG "5 5 ";
-	### HOT - Red
-	} elsif ($sector{type} eq "hot" or $sector{type} eq "chernobyl") {
-		print XFIG "4 4 ";
-	} else {
-		print "ERROR: Unknown type: ",$sector{type},"\n";
-	}
-
+	my $color = get_color($sector{type});
+	print XFIG "$color $color ";
 	### Depth is just 0 for now
 	print XFIG "0 ";
-
 	### Pen style, not used
 	print XFIG "-1 ";
-
 	### Area fill - No fill
 	print XFIG "-1 ";
-
 	### Style value
 	print XFIG "0.000 ";
-
 	### Join style - Miter
 	print XFIG "0 ";
-
 	### Cap style - unused
 	print XFIG "0 ";
-
 	### Radius - unused
 	print XFIG "-1 ";
-
 	### Arrows -> off
 	print XFIG "0 0 ";
-
 	### Number of points in the line
 	print XFIG $sector{numvertices} + 1,"\n";
-
 	### Now loop through the vertices
-	my @verts = @{$sector{verts}};
+	my @verts = @{conv_verts($sector{verts}, $scale, \@lowest, $game)};
 	foreach my $v (@verts) {
-		my $x = $v->[0];
-		my $y = $v->[1];
-		my $z = $v->[2];
-
-		### EMI
-		if ($game eq "EMI") {
-			print XFIG $lowest[0] + int($x * 1000), " ", $lowest[2] + int($z * 1000), "\n";
-		### GRIM
-		} else {
-			print XFIG $lowest[0] + int($x * 1000), " ", $lowest[1] + int($y * 1000), "\n";
-		}
+		print XFIG $v->[0], " ", $v->[1], "\n";
 	}
-	### Return to the first vert
-	my $x = $verts[0]->[0];
-	my $y = $verts[0]->[1];
-	my $z = $verts[0]->[2];
 
-	### EMI
-	if ($game eq "EMI") {
-		print XFIG $lowest[0] + int($x * 1000), " ", $lowest[2] + int($z * 1000), "\n";
-	### GRIM
-	} else {
-		print XFIG $lowest[0] + int($x * 1000), " ", $lowest[1] + int($y * 1000), "\n";
-	}
+	### Draw the text for this sector
+	print XFIG "# Text object for ", $sector{name}, " ($k)\n";
+	### Text object is always 4
+	print XFIG "4 ";
+	### Centered
+	print XFIG "1 ";
+	### Color
+	print XFIG "$color ";
+	### Depth
+	print XFIG "0 ";
+	### Pen Style - unused
+	print XFIG "-1 ";
+	### Font - Courier
+	print XFIG "12 ";
+	### Font Size
+	print XFIG "10 ";
+	### Angle (in radians)
+	print XFIG "0.0000 ";
+	### Font Flags - Sans Serif
+	print XFIG "4 ";
+	### Height, Length
+	print XFIG "105 ", 90 * length($k), " ";
+	### X, Y of Center
+	my ($text_x, $text_y) = bb_center(\@verts);
+	print XFIG "$text_x $text_y ";
+	### Text (sector ID), terminated by \001
+	print XFIG $k,"\\001\n";
 }
 close XFIG;
